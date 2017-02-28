6473f6fdac9bd7e72396a338ae421324b6d5302e
Ryan Mathisen <ryantmathisen@gmail.com>
2017-02-27 16:26:17 -0800

    add large amount of improvements, including a proper grid system, gate-grid coordinate system that updates dynamically when a gate is dropped on a grid or changed from one grid square to another, and dynamically add/remove disabled property to grid squares based on user input (i.e. if a user drags a gate from one square to another, that first square will not be disabled any more, but if the second square is disabled, the first square will properly be disabled on revert)

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,59 @@ $(function() {
 
   InitializeBoard();
 
-  $(".gate").draggable( {snap: true, containment: $("#square-container"), revert: "invalid", revertDuration: 200 });
+  $(".gate").draggable( {
+    containment: $("#square-container"),
+    revert: function(droppedObj) {
+      if (!droppedObj) {
+        droppable = $("#" + $(this).attr('value'));
+        droppable.addClass("disabled");
+        droppable.droppable("option", "disabled", true);
+        return true;
+      }
+    },
+    revertDuration: 200,
+    start: function() {
+      //If gate starts being moved from where it was dropped, remove that square's diabled class and 'disabled' attribute
+      var droppable = $("#" + $(this).attr('value'));
+      droppable.removeClass("disabled");
+      droppable.droppable( "option", "disabled", false );
+    }
+  });
   $(".grid").droppable( {
     accept: '.ui-draggable',
     drop: dropGatePiece
-});
-
-  function dropGatePiece(event, ui) {
-    $(this).addClass("ui-state-highlight");
-    ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
-  }
+  });
+  $("#gate-container").droppable( {
+    accept: '.ui-draggable',
+    drop: dropGatePiece
+  });
 })
 
+//Regex for getElemCoords()
+var onlyNumbers = /\d+/g;
+
+function getElemCoords (idString) {
+  return idString.match(onlyNumbers);
+}
+
 function InitializeBoard() {
-  for (var i = 0; i < 5; i++) {
-    $('#square-container').append("<div class='.row'>");
+  for (var i = 0; i < 10; i++) {
+    $('#square-container').append("<div id='row" + i + "' class='row'>");
     for (var j = 0; j < 10; j++) {
-      $('#square-container').append("<div class='.grid'></div>")
+      $('#row' + i).append("<div id='" + i + "-" + j + "' class='grid'></div>")
     }
     $('#square-container').append("</div>");
+  }
+}
+
+function dropGatePiece(event, ui) {
+  draggable = $("#" + ui.draggable.attr('id'));
+  var droppableId = $(this).attr('id');
+  draggable.attr('value', droppableId);
+
+  if ($(this).attr('id') != "gate-container") {
+    ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+    $(this).droppable( "option", "disabled", true );
+    $(this).addClass("disabled");
   }
 }
